16ebc017c1493b76b090cedc69aa9b83c9a92898
andrademiguel <miguel4andrade@gmail.com>
2020-11-23 19:12:05 -0800

    steps 8 and 9

--- AppTest/index.js
+++ AppTest/index.js
@@ -90,3 +90,49 @@ app.get("/edit/:id", (req, res) => {
         res.render("edit", { model: row });
     });
 });
+
+// POST /edit/5
+app.post("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const book = [req.body.Title, req.body.Author, req.body.Comments, id];
+    const sql = "UPDATE Books SET Title = ?, Author = ?, Comments = ? WHERE (Book_ID = ?)";
+    db.run(sql, book, err => {
+        // if (err) ...
+        res.redirect("/books");
+    });
+});
+
+// GET /create
+app.get("/create", (req, res) => {
+    res.render("create", { model: {} });
+});
+
+// POST /create
+app.post("/create", (req, res) => {
+    const sql = "INSERT INTO Books (Title, Author, Comments) VALUES (?, ?, ?)";
+    const book = [req.body.Title, req.body.Author, req.body.Comments];
+    db.run(sql, book, err => {
+        // if (err) ...
+        res.redirect("/books");
+    });
+});
+
+// GET /delete/5
+app.get("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM Books WHERE Book_ID = ?";
+    db.get(sql, id, (err, row) => {
+      // if (err) ...
+      res.render("delete", { model: row });
+    });
+  });
+
+  // POST /delete/5
+app.post("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "DELETE FROM Books WHERE Book_ID = ?";
+    db.run(sql, id, err => {
+      // if (err) ...
+      res.redirect("/books");
+    });
+  });
